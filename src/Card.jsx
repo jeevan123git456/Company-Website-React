c864ddf9995acf48e1576e4bc576a1b81470b219
Jeevan_Bhuyan <jeevanbhuyan@gmail.com>
2024-02-26 12:02:12 +0530

    new bootstrap changed added

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,15 +6,22 @@ function Card({ title, imgsrc }) {
     <>
       <div className="col-md-4 col-10 mx-auto">
         <div className="card">
-          <img src={imgsrc} className="card-img-top" alt="..." />
+          <img
+            src={imgsrc}
+            style={{ height: "18rem" }}
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title" style={{ height: "3rem" }}>
+              {title}
+            </h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <NavLink to="/contact" className="btn btn-primary">
-              Get Quote
+              Explore
             </NavLink>
           </div>
         </div>
--- src/Service.js
+++ src/Service.js
@@ -2,10 +2,10 @@ import React from "react";
 import Card from "./Card";
 import img1 from "../src/Pictures/img1.png";
 import img2 from "../src/Pictures/img2.png";
-import img3 from "../src/Pictures/img3.png";
-import img4 from "../src/Pictures/img4.png";
-import img5 from "../src/Pictures/img5.png";
-import img6 from "../src/Pictures/img6.png";
+// import img3 from "../src/Pictures/img3.png";
+// import img4 from "../src/Pictures/img4.png";
+// import img5 from "../src/Pictures/img5.png";
+// import img6 from "../src/Pictures/img6.png";
 
 function Service() {
   return (
@@ -14,16 +14,16 @@ function Service() {
         <h1 className="text-center">Our Services</h1>
       </div>
 
-      <div className="container-fluid mb-5">
+      <div className="container-fluid mb-10 ">
         <div className="row">
           <div className="col-10 mx-auto">
             <div className="row gy-4">
-              <Card title="Android Development" imgsrc={img1} />
-              <Card title="Web Development" imgsrc={img2} />
-              <Card title="Database Management" imgsrc={img3} />
+              <Card title="IT Services" imgsrc={img1} />
+              <Card title="Mechanical Services" imgsrc={img2} />
+              {/* <Card title="Database Management" imgsrc={img1} />
               <Card title="Business Analytics" imgsrc={img4} />
               <Card title="Digital Marketing" imgsrc={img5} />
-              <Card title="Video Creation" imgsrc={img6} />
+              <Card title="Video Creation" imgsrc={img6} /> */}
             </div>
           </div>
         </div>
